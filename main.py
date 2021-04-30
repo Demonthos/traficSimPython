d37bfebaf9d7323f34ec7601fc5e0f79c0ad2765
Demonthos <66571940+Demonthos@users.noreply.github.com>
2021-04-29 19:33:19 -0500

    finished road class and added intersections

--- main.py
+++ main.py
@@ -2,51 +2,232 @@ import tkinter as tk
 import random
 import itertools
 import math
+from iteration_utilities import flatten
+
+size = [500] * 2
+
+
+def _from_rgb(rgb):
+    """translates an rgb tuple of int to a tkinter friendly color code
+    """
+    r, g, b = rgb
+    return f'#{r:02x}{g:02x}{b:02x}'
 
 
 class Car:
     def __init__(self, position, size, canvas):
-        self.obj = canvas.create_oval(position[0], position[1], size, size, width=2, fill='red')
+        self.size = size
+        self.obj = canvas.create_oval(position[0], position[1], position[0] - size, position[1] - size, width=2,
+                                      fill='red')
 
     def move(self, point, canvas):
         canvas.move(self.obj, point[0], point[1])
 
-    def follow(self, road, canvas):
-        for line in road.lines:
-            lengthOfLine = int(math.sqrt((line.point1[0] - line.point2[0])**2 + (line.point1[1] - line.point2[1])**2))
-            for i in range(0, lengthOfLine):
-                positionInLine = (i/lengthOfLine)*(line.point2[1] - line.point1[1])
-                print(positionInLine)
-                slope = (line.point1[1] - line.point2[1])/(line.point1[0] - line.point2[0])
-                position = (positionInLine - line.point1[1], slope*(positionInLine - line.point1[0]))
-                canvas.after(100*i, self.move, position, canvas)
+    def moveTo(self, point, canvas):
+        canvas.moveto(self.obj, point[0], point[1])
+
+    def drive(self, line, currentDelay, canvas, forward=True):
+        canvas.after(currentDelay, lambda: line.addCar(self, canvas))
+        lengthOfLine = int(line.length)
+        startingPos = line.point1
+        sizeOffset = [self.size / 2] * 2
+        movement = (line.xLength, line.yLength)
+        for i in (range(0, lengthOfLine) if forward else range(lengthOfLine, 0, -1)):
+            newPos = [((i / lengthOfLine) * m) + p - s for m, p, s in
+                      zip(movement, startingPos, sizeOffset)]
+            canvas.after(currentDelay, self.moveTo, newPos, canvas)
+            currentDelay += 10
+            # print(newPos)
+        canvas.after(currentDelay, lambda: line.removeCar(self, canvas))
+        return currentDelay
+
+    def changeRoad(self, currentRoad, canvas, forward=True):
+        if (not forward) in currentRoad.connected.keys():
+            currentIntersection = currentRoad.connected[not forward]
+            callback = lambda: self.follow(random.choice(list(currentIntersection.roads - set([currentRoad]))), canvas,
+                                           forward)
+            currentIntersection.wait(callback, canvas)
+        else:
+            self.follow(currentRoad, canvas, not forward)
+        # print(currentRoad.connected, forward)
+
+    def follow(self, road, canvas, forward=True):
+        currentDelay = 0
+        for line in (road.lines if forward else road.lines[::-1]):
+            currentDelay = self.drive(line, currentDelay, canvas, forward)
+        canvas.after(currentDelay, self.changeRoad, road, canvas, forward)
 
 
 class Line:
     def __init__(self, point1, point2, canvas):
-        self.point1 = point1
-        self.point2 = point2
-        self.obj = canvas.create_line(point1[0], point1[1], point2[0], point2[1], width=2, fill='red')
+        self.point1: tuple = point1
+        self.point2: tuple = point2
+        self.obj = canvas.create_line(point1[0], point1[1], point2[0], point2[1], width=10)
+        self.carsOn: set = set()
+        self.updateColor(canvas)
+
+    @property
+    def xLength(self):
+        return self.point2[0] - self.point1[0]
+
+    @property
+    def yLength(self):
+        return self.point2[1] - self.point1[1]
+
+    @property
+    def length(self):
+        return math.sqrt(self.xLength ** 2 + self.yLength ** 2)
+
+    @property
+    def slope(self):
+        return self.yLength / self.xLength
+
+    def updateColor(self, canvas):
+        canvas.itemconfig(self.obj, fill=_from_rgb((10 * len(self.carsOn), 0, 0)))
+
+    def addCar(self, car, canvas):
+        self.carsOn.add(car)
+        self.updateColor(canvas)
+
+    def removeCar(self, car, canvas):
+        self.carsOn.remove(car)
+        self.updateColor(canvas)
+
+    def delete(self, canvas):
+        canvas.delete(self.obj)
 
 
 class Road:
-    def __init__(self, lines, canvas):
-        self.lines = lines
+    def __init__(self, points, canvas):
+        self.lines = [Line(points[i], points[i + 1], canvas) for i in range(0, len(points) - 1)]
+        self.connected_ = {}
 
+    @property
+    def carsOn(self):
+        total = set()
+        for line in self.lines:
+            total |= line.carsOn
+        return total
+
+    def addConnection(self, intersection, canvas, start=True):
+        self.connected_[start] = intersection
+        intersection.addConnection(self, canvas, start)
+
+    @property
+    def connected(self):
+        return self.connected_
+
+    def delete(self, canvas):
+        for line in self.lines:
+            line.delete(canvas)
+
+
+class Intersection:
+    def __init__(self, size):
+        self.size = size
+        self.roads = set()
+        self.obj = None
+        self.delay = 100
+
+    def getRandom(self):
+        return random.choice(list(self.roads))
+
+    def addConnection(self, road, canvas, start):
+        position = road.lines[0].point1 if start else road.lines[-1].point2
+        position = [p + (self.size / 2) for p in position]
+        # if not self.obj:
+        self.obj = canvas.create_oval(position[0], position[1], position[0] - self.size, position[1] - self.size,
+                                      width=2,
+                                      fill='blue')
+        self.roads.add(road)
+        # print(position, self.roads, start, road.connected)
+
+    def wait(self, callback, canvas):
+        canvas.after(self.delay, callback)
 
 top = tk.Tk()
 
-C = tk.Canvas(top, bg="white", height=500, width=500)
+C = tk.Canvas(top, bg="white", height=size[0], width=size[1])
 
-# points = (0, 0), (0, 100), (100, 0)
-# image = C.create_polygon(list(itertools.chain(*points)))
-car = Car((100, 100), 20, C)
-road = Road([Line((0, 0), (300, 300), C)], C)
-car.follow(road, C)
+
+def genRoad(genLength, currentState=None, pointsOccupied=[]):
+    points = [currentState] if currentState else []
+    while len(points) < genLength:
+        newPoint = False
+        repeatCount = 0
+        while (not newPoint) or newPoint in points or newPoint in pointsOccupied or not (
+                size[0] > newPoint[0] > 0 and size[1] > newPoint[1] > 0):
+            # if their is no valid move undo last move
+            if repeatCount > 100 + ((len(points) == 0) * 1000):
+                if len(points) > 0:
+                    points.pop()
+                else:
+                    # no possible valid moves
+                    return None
+            newPoint = tuple(p + (30 * (random.randint(0, 2) - 1)) for p in points[-1]) if len(points) > 0 else (
+                size[0] / 2, size[1] / 2)
+            repeatCount += 1
+        points.append(newPoint)
+    return points
+
+
+roads = []
+numSegmentsPerRoad = 3
+numLights = 50
+allPoints = [None]
+newSegments = None
+retryCount = 0
+
+
+def goBack():
+    global allPoints, roads, retryCount
+    retryCount += 1
+    if len(roads) > 0:
+        # recalculate all points
+        roads.pop().delete(C)
+        allPoints = list(set(flatten(flatten([[(line.point1, line.point2) for line in road.lines] for road in roads]))))
+        return True
+    else:
+        return False
+
+
+while len(roads) < numLights:
+    # retried a lot
+    if retryCount > 500:
+        while len(roads) > 0:
+            roads.pop().delete(C)
+        numSegmentsPerRoad = 5
+        allPoints = [None]
+        newSegments = None
+        retryCount = 0
+    if newSegments is not None:
+        newRoad = Road(newSegments, C)
+        roads.append(newRoad)
+        allPoints += newSegments
+    newSegments = genRoad(numSegmentsPerRoad, allPoints[-1], allPoints)
+    # no valid moves go back
+    if newSegments is None:
+        if goBack():
+            print('going back one road', len(roads), len(allPoints))
+
+pRoad = None
+for road in roads:
+    if pRoad:
+        intersection = Intersection(20)
+        road.addConnection(intersection, C, True)
+        pRoad.addConnection(intersection, C, False)
+    pRoad = road
+cars = [Car((100, 100), 10, C) for i in range(1, 30)]
+for i, car in enumerate(cars):
+    if i % 2 == 0:
+        car.follow(roads[random.randint(0, len(roads) - 1)], C, True)
+    else:
+        car.follow(roads[random.randint(0, len(roads) - 1)], C, False)
 
 
 def move(event):
-    car.move((random.randint(-10, 10), random.randint(-10, 10)), C)
+    # change visualization mode here
+    pass
 
 
 top.bind("<Key>", move)
